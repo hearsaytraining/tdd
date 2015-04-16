5932c1db17cf180be17bfe9f1c8cdf90d3e468b7
Hearsay Training <training@Hearsay-Training.local>
2015-04-15 17:17:27 -0700

    Three changed files plus two migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,33 +3,46 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.models import Item
+from lists.models import List
 from lists.views import home_page
 from django.template.loader import render_to_string
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+        def test_saving_and_retrieving_items(self):
+                list_ = List()
+                list_.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
+                first_item = Item()
+                first_item.text = 'The first (ever) list item'
+                first_item.list = list_
+                first_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+                second_item = Item()
+                second_item.text = 'Item the second'
+                second_item.list = list_
+                second_item.save()
 
-		self.assertEqual(saved_items[0].text, 'The first (ever) list item')
-		self.assertEqual(saved_items[1].text, 'Item the second')
-		
+                saved_list = List.objects.first()
+                self.assertEqual(saved_list, list_)
+
+                saved_items = Item.objects.all()
+                self.assertEqual(saved_items.count(), 2)
+
+                first_saved_item = saved_items[0]
+                second_saved_item = saved_items[1]
+                self.assertEqual(saved_items[0].text, 'The first (ever) list item')
+                self.assertEqual(first_saved_item.list, list_)
+                self.assertEqual(saved_items[1].text, 'Item the second')
+                self.assertEqual(second_saved_item.list, list_)
+                
 
 class HomePageTest(TestCase):
 
 # Create your tests here.
         def test_root_url_resolves_to_home_page_view(self):
-        	found = resolve('/')
-        	self.assertEqual(found.func, home_page)
+                found = resolve('/')
+                self.assertEqual(found.func, home_page)
 
         def test_home_page_returns_correct_html(self):
                 request = HttpRequest()
@@ -44,13 +57,10 @@ class ListViewTest(TestCase):
 
 
         def test_displays_all_items(self):
-                Item.objects.create(text='itemey 1')
-                Item.objects.create(text='itemey 2')
+                list_ = List.objects.create()
+                Item.objects.create(text='itemey 1', list=list_)
+                Item.objects.create(text='itemey 2', list=list_)
 
-                response = self.client.get('/lists/the-only-list-in-the-world/')
-
-                self.assertContains(response, 'itemey 1')
-                self.assertContains(response, 'itemey 2')
 
 class NewListTest(TestCase):
         def test_saving_a_POST_request(self):
